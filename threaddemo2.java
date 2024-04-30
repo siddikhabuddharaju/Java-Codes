d618420d1e33747a1eecd6d6fe41c1177432a8ac
siddikhabuddharaju <150932314+siddikhabuddharaju@users.noreply.github.com>
2024-04-30 19:11:08 +0530

    threaddemo2.java

---
+++ threaddemo2.java
@@ -0,0 +1,19 @@
+public class threaddemo2 implements Runnable 
+{
+    String name;
+    threaddemo2(String nam)
+    {
+        name=nam;
+    }
+    public threaddemo2() {}
+    public void run(){System.out.println("thread run method started by "+name);}
+    public void start(){System.out.println("thread execution started");}
+    public static void main(String args[])
+    {
+        threaddemo2 obj=new threaddemo2("gayi");
+        Thread t1=new Thread(obj);
+        //Thread t2=new Thread("yamu");
+        t1.run();
+        t1.start();
+    }
+}
